24c5ec3d0146891aa7240a8e6d11400b067518f8
Clavis-securite <clavis.service.client@gmail.com>
2026-01-22 00:14:42 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    IMPORTANT : incrémente CACHE_VERSION à chaque changement
 */
 
-const CACHE_VERSION = "v22"; // ⬅️ bump après correctif coffre (évite écran noir)
+const CACHE_VERSION = "v23"; // ⬅️ bump après correctif coffre (évite écran noir)
 const STATIC_CACHE = `clavis-static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `clavis-runtime-${CACHE_VERSION}`;
 
